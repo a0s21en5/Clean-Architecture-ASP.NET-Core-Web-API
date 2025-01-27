c8b5d7901aa7bfd02e921e75eeed4fcd93b96ed0
Ashish Sahu <ashsahu9713@gmail.com>
2025-01-27 09:31:31 +0530

    Update Clean Architecture ASP.NET Core Web API

---
+++ BookStore.APIs/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Application;
+using BookStore.Infrastructure;
+
+namespace BookStore.APIs
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBookStoreDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI().AddInfrastructureDI();
+            return services;
+        }
+    }
+}
---
+++ BookStore.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BookStore.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationDI(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
